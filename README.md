69143d47e378d1e6c5ff6c12b33fb3a49984ee05
jwong-nd <jonathan.wong@alleninstitute.org>
2023-05-12 19:06:02 +0000

    jax/cuda install, gpu normalization, elasticreg

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # sofima-testing
 
 Sofima testing code. 
+
+
+## Development Environment Setup Instructions from environment.yml
+1) Setup conda environment: 
+    ```
+    conda env create -f environment.yml
+    ```
+
+2) Run the following commands to install the compatible cuDNN library to this JAX version specified in the environment.yml file:
+    ```
+    $ tar -xvf cudnn-linux-x86_64-8.9.0.131_cuda11-archive.tar.xz
+    $ sudo cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include 
+    $ sudo cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64 
+    $ sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
+    ```
+
+## Development Environment Setup Instructions for Scratch
+1) Create Python 3.11 conda environment  
+2) Pip install standard packages. Exceptions: 
+    - SOFIMA is in active development, pip install the most recent commit: 
+    ```
+    pip install git+https://github.com/google-research/sofima
+    ```
+    - JAX has dependencies on (GPU driver version, CUDA middleware version, cuDNN library version). 
+    This command gets you halfway there, which installs the most up-to-date drivers and Jax version compatible to your locally-installed CUDA version. 
+    ```
+    conda install jaxlib=*=*cuda* jax cuda-nvcc -c conda-forge -c nvidia
+    ```
+
+3) Last JAX dependency, cuDNN library version, is pulled from the NVIDIA website (you need to first sign up for an NVIDIA account). Downloaded cuDNN library must match the major version and be greater than the minor version of the cuDNN compiled into JAX. (Essentially, just download the newest version.)
+
+Downloaded File looks something like this: ```cudnn-linux-x86_64-8.9.0.131_cuda11-archive.tar.xz```
+Simply copy the zip file onto the VM, unzip, and copy into base folders:
+    ```
+    $ tar -xvf cudnn-linux-x86_64-8.9.0.131_cuda11-archive.tar.xz
+    $ sudo cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include 
+    $ sudo cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64 
+    $ sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
+    ```
--- data_prep.py
+++ data_prep.py
@@ -1,7 +1,8 @@
-from concurrent import futures
 import numpy as np
-import skimage.exposure 
 import tensorstore as ts
+
+import kornia
+import torch
 
 import zarr_io
 
@@ -25,19 +26,52 @@ MAX_BRIGHTNESS = 800
 #             for f in futures.as_completed(fs):
 #                 f.result()
 
-
-def preprocess_tiles(tile_volumes: list[ts.TensorStore], output_names: list[str], bucket: str):
-    with futures.ThreadPoolExecutor() as tpe: 
-        for src_vol, output_name in zip(tile_volumes, output_names):
-            out_vol = zarr_io.write_zarr(bucket, src_vol.shape, output_name)
-            fs = set()
-            for z in range(out_vol.shape[2]):  # Iterate through z planes
-                def _clahe(z):
-                    sec = src_vol[0, 0, z, :, :].read().result()
-                    sec = np.clip(sec, MIN_BRIGHTNESS, MAX_BRIGHTNESS)
-                    clahed = skimage.exposure.equalize_adapthist(sec, clip_limit=0.03)
-                    out_vol[0, 0, z, :, :].write((clahed * 255).astype(np.uint8)).result()
-                fs.add(tpe.submit(_clahe, z))
+# def preprocess_tiles(tile_volumes: list[ts.TensorStore], output_names: list[str], bucket: str):
+#     with futures.ThreadPoolExecutor() as tpe: 
+#         for src_vol, output_name in zip(tile_volumes, output_names):
+#             out_vol = zarr_io.write_zarr(bucket, src_vol.shape, output_name)
+#             fs = set()
+#             for z in range(out_vol.shape[2]):  # Iterate through z planes
+#                 def _clahe(z):
+#                     sec = src_vol[0, 0, z, :, :].read().result()
+#                     sec = np.clip(sec, MIN_BRIGHTNESS, MAX_BRIGHTNESS)
+#                     clahed = skimage.exposure.equalize_adapthist(sec, clip_limit=0.03)
+#                     out_vol[0, 0, z, :, :].write((clahed * 255).astype(np.uint8)).result()
+#                 fs.add(tpe.submit(_clahe, z))
             
-            for f in futures.as_completed(fs):
-                f.result()
+#             for f in futures.as_completed(fs):
+#                 f.result()
+
+def preprocess_tiles(tile_volumes: list[ts.TensorStore], output_names: list[str], bucket: str, batch_size: int=32):
+    BATCH_SIZE = batch_size
+    for src_vol, output_name in zip(tile_volumes, output_names):  # Iterate through volumes
+        out_vol = zarr_io.write_zarr(bucket, src_vol.shape, output_name)
+
+        _, _, z, y, x = out_vol.shape
+        num_batches = int(np.ceil(z / BATCH_SIZE))
+        
+        for i in range(num_batches - 1):  # Iterate through batches of planes
+            batch_index = i * BATCH_SIZE
+            sec = src_vol[0, 0, batch_index:batch_index + BATCH_SIZE + 1, :, :].read().result()
+            sec = np.clip(sec, MIN_BRIGHTNESS, MAX_BRIGHTNESS)
+            
+            # Formatting to (B, C, H, W): 
+            # (1, z, y, x) -> (z, 1, y, x)
+            sec_z, _, _ = sec.shape  
+            sec = sec[np.newaxis, :]
+            sec = sec.reshape(sec_z, 1, y, x)
+            sec = np.float16(sec)
+            sec = torch.tensor(sec, device='cuda')
+            
+            sec = kornia.enhance.normalize_min_max(sec)
+            clahed = kornia.enhance.equalize_clahe(sec)  # Keeping default parameters
+
+            # Formatting back to (z, y, x)
+            clahed = sec.cpu().numpy()
+            clahed = clahed.reshape(1, sec_z, y, x)
+            clahed = clahed[0, :, :, :]
+
+            clahed = clahed * 255
+            clahed = np.uint8(clahed) 
+
+            out_vol[0, 0, batch_index:batch_index + BATCH_SIZE + 1, :, :].write(clahed).result()
